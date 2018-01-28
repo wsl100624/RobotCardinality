3b88981e4d0110f4edf57c7c690664efeb0e58e1
wsl100624 <wang3027@stthomas.edu>
2018-01-28 09:06:22 -0600

    Initial Commit

---
+++ RobotCardinality/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RobotCardinality
+//
+//  Created by Wang Shilong on 1/28/18.
+//  Copyright © 2018 Wang Shilong. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
